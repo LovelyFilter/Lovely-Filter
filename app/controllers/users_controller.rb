2f2a829771fb5cf88256f23c69b64f3dea1699b7
jeffmorrisjr <jeff@zaarly.com>
2015-01-27 14:24:49 -0800

    authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,23 @@
 class UsersController < ApplicationController
-  def index
+  before_filter :logged_in?, only: [:profile]
+
+  def profile
+    @user = current_user
+    render "users/profile"
   end
 
-  def new
-  end
-
-  def show
-  end
-
-  def edit
-  end
 end
+
+# class UsersController < ApplicationController
+#   def index
+#   end
+
+#   def new
+#   end
+
+#   def show
+#   end
+
+#   def edit
+#   end
+# end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,50 @@
 module SessionsHelper
+  # Assuming you have the route in routes.rb
+  # get "/oauth/callback", to: "sessions#callback"
+  CALLBACK_URL = "http://localhost:3000/oauth/callback"
+
+  # also on instagram's API management 
+  #   you should configure your oauth
+  #   callback to match the one above,
+  #   at least while in development.
+
+  Instagram.configure do |config|
+    config.client_id = ENV["CLIENT_ID"] # <--- NOTE THE CHANGE
+    config.client_secret = ENV["CLIENT_SECRET"] #<---- NOTE THE CHANGE
+    # For secured endpoints only
+    #config.client_ips = '<Comma separated list of IPs>'
+  end
+
+    
+  def instagram_authorize()
+     redirect_to Instagram.authorize_url(:redirect_uri => CALLBACK_URL)
+  end
+
+  def login(code)
+      # Once user authenticates
+    #  their access_token
+    #  will be made available
+    #  in the response to this
+    #  method...
+    res = Instagram.get_access_token(code, redirect_uri: CALLBACK_URL)
+    # The access token 
+    # is then put into the
+    # session
+    session[:access_token] = res.access_token
+  end
+
+  def logout
+    session[:access_token] = nil
+  end
+
+  def logged_in?
+    if session[:access_token].nil?
+      redirect_to root_path
+    end
+  end
+
+  def current_user
+    client ||= Instagram.client(:access_token => session[:access_token])
+    client.user
+  end
 end
